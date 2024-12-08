cf77f19325aa88b673d5b7f5bb587ba7d68997d1
akashmahlax <akashdalla407@gmail.com>
2024-12-08 16:54:01 +0530

    profile pic added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,9 +10,12 @@ export default function Navbar() {
       <div className="container mx-auto px-6">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
-          <Link href="/" className="text-2xl font-bold text-white">
-            Akash
-          </Link>
+          <div className='flex items-center'>
+            <img src="./IMG_2370.jpg" alt="logo" className='w-20 h-20 m-2 p-2 rounded-full object-cover' />
+            <Link href="/" className="text-2xl font-bold font-mono px-2 text-white">
+              Akash
+            </Link>
+          </div>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-8">
@@ -34,7 +37,7 @@ export default function Navbar() {
           </div>
 
           {/* Mobile Menu Button */}
-          <button 
+          <button
             className="md:hidden text-gray-300"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
@@ -52,36 +55,36 @@ export default function Navbar() {
         {isMenuOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              <Link 
-                href="/" 
+              <Link
+                href="/"
                 className="block px-3 py-2 text-gray-300 hover:text-white transition"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Home
               </Link>
-              <Link 
-                href="/about" 
+              <Link
+                href="/about"
                 className="block px-3 py-2 text-gray-300 hover:text-white transition"
                 onClick={() => setIsMenuOpen(false)}
               >
                 About
               </Link>
-              <Link 
-                href="/projects" 
+              <Link
+                href="/projects"
                 className="block px-3 py-2 text-gray-300 hover:text-white transition"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Projects
               </Link>
-              <Link 
-                href="/contact" 
+              <Link
+                href="/contact"
                 className="block px-3 py-2 text-gray-300 hover:text-white transition"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Contact
               </Link>
-              <Link 
-                href="/intro" 
+              <Link
+                href="/intro"
                 className="block px-3 py-2 text-gray-300 hover:text-white transition"
                 onClick={() => setIsMenuOpen(false)}
               >
